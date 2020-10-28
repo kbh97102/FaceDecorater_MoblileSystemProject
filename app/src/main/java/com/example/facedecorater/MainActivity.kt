daf081db7244dfdf74d94742df4e32ac15847edb
Arakene <kbh97102@naver.com>
2020-10-28 19:53:32 +0900

    Remove unnecessary code

--- app/src/main/java/com/example/facedecorater/MainActivity.kt
+++ app/src/main/java/com/example/facedecorater/MainActivity.kt
@@ -1,39 +1,16 @@
 package com.example.facedecorater
 
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.ContextMenu
 import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.facedecorater.ar.ArTest
 import kotlinx.android.synthetic.main.gallery_menu_layout.*
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private val PERMISSIONS = arrayOf(
-            android.Manifest.permission.INTERNET, android.Manifest.permission.ACCESS_NETWORK_STATE,
-            android.Manifest.permission.CAMERA
-        )
-        private const val REQUEST_CODE = 1
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gallery_menu_layout)
 
-//        if (checkPermissionIsGranted()) {
-//            val intent = Intent(this, ArTest::class.java)
-//            startActivity(intent)
-//        } else {
-//            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE)
-//        }
         my_toolbar.apply {
             title = ""
             subtitle = ""
@@ -42,39 +19,9 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    private fun checkPermissionIsGranted() = PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.gallery_menu, menu)
         return true
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == REQUEST_CODE) {
-            if (!checkPermissionIsGranted()) {
-                val alertBuilder = AlertDialog.Builder(this)
-                alertBuilder.apply {
-                    setTitle("Permissions are rejected")
-                    setMessage("Please Allow Permissions")
-                    setPositiveButton("Yes") { _, _ ->
-                        ActivityCompat.requestPermissions(
-                            this@MainActivity,
-                            PERMISSIONS,
-                            REQUEST_CODE
-                        )
-                    }
-                }.create().apply {
-                    setCanceledOnTouchOutside(false)
-                }.run { show() }
-            }
-        }
-    }
-
 }
