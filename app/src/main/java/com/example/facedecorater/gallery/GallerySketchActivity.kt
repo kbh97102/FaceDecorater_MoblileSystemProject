6dff652fa10943c25b933f741086761afa7be2fd
Arakene <kbh97102@naver.com>
2020-11-04 20:18:51 +0900

    Add setToolbar

--- app/src/main/java/com/example/facedecorater/gallery/GallerySketchActivity.kt
+++ app/src/main/java/com/example/facedecorater/gallery/GallerySketchActivity.kt
@@ -1,6 +1,25 @@
 package com.example.facedecorater.gallery
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.facedecorater.R
+import kotlinx.android.synthetic.main.gallery_sketch_layout.*
 
 class GallerySketchActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gallery_sketch_layout)
+
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        gallery_sketch_toolbar.apply {
+            title = ""
+            subtitle = ""
+        }
+        setSupportActionBar(gallery_sketch_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 }
