9bc69104dc90da667ecf8b2ce712a6af5c308fdf
Arakene <kbh97102@naver.com>
2020-11-04 16:14:21 +0900

    Add defalut Sticker

--- app/src/main/java/com/example/facedecorater/gallery/GalleryStickerActivity.kt
+++ app/src/main/java/com/example/facedecorater/gallery/GalleryStickerActivity.kt
@@ -1,15 +1,22 @@
 package com.example.facedecorater.gallery
 
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.example.facedecorater.R
 import kotlinx.android.synthetic.main.gallery_sticker_layout.*
 
+@SuppressLint("ClickableViewAccessibility")
 class GalleryStickerActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +24,50 @@ class GalleryStickerActivity : AppCompatActivity() {
 
 
 
+        gallery_heartSticker.setOnClickListener {
+            addSticker(R.drawable.heart_sticker)
+        }
+        gallery_starSticker.setOnClickListener {
+            addSticker(R.drawable.star_sticker)
+        }
     }
 
-    private fun addButtonInRuntime(){
+    private fun addSticker(src: Int) {
+        var x: Double? = 0.0
+        var y: Double? = 0.0
+        var sticker = ImageView(this)
+        var stickerBitmap = BitmapFactory.decodeResource(resources, src)
+        sticker.setImageBitmap(stickerBitmap)
+        sticker.setBackgroundColor(Color.TRANSPARENT)
+        sticker.setOnTouchListener { view, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    x = view.x.toDouble() - event.rawX
+                    y = view.y.toDouble() - event.rawY
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    sticker.animate()
+                        .x(event.rawX + x!!.toFloat())
+                        .y(event.rawY + y!!.toFloat())
+                        .setDuration(0)
+                        .start()
+                    true
+                }
+                else -> true
+            }
+        }
+        gallery_sticker_layout.addView(sticker)
+    }
+
+    private fun addButtonInRuntime() {
         var button = Button(this).apply {
             id = View.generateViewId()
             text = "testButton"
-            layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams = ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+            )
         }
 
         var constraintSet = ConstraintSet()
