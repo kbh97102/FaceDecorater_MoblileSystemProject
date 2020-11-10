db0a1cfbaa6226cba91822f5913f4746accb1e06
Arakene <kbh97102@naver.com>
2020-11-10 16:19:15 +0900

    Update augumentFace

---
+++ app/src/main/java/com/example/facedecorater/camera/AugmentFace.kt
@@ -0,0 +1,39 @@
+package com.example.facedecorater.camera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.facedecorater.R
+import com.google.ar.core.AugmentedFace
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Texture
+import com.google.ar.sceneform.ux.AugmentedFaceNode
+
+class AugmentFace : AppCompatActivity() {
+
+    private lateinit var arFragment: FaceArFragment
+    private lateinit var faceRegionsRenderable: ModelRenderable
+    private lateinit var faceMeshTexture: Texture
+    private val faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_ar)
+
+//
+//        //https://developers.google.com/ar/develop/java/camera-configs
+//
+//
+//        val sharedSession = Session(this, EnumSet.of(Session.Feature.SHARED_CAMERA))
+//        val sharedCamera = sharedSession.sharedCamera
+//        val filter = CameraConfigFilter(sharedSession)
+//        filter.apply {
+//            targetFps = EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30)
+//            depthSensorUsage = EnumSet.of(CameraConfig.DepthSensorUsage.DO_NOT_USE)
+//        }
+//        val cameraConfigList = sharedSession.getSupportedCameraConfigs(filter)
+//        sharedSession.cameraConfig = cameraConfigList[0]
+//        val cameraId = sharedSession.cameraConfig.cameraId
+
+    }
+}
---
+++ app/src/main/java/com/example/facedecorater/camera/FaceArFragment.kt
@@ -0,0 +1,42 @@
+package com.example.facedecorater.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.google.ar.core.Config
+import com.google.ar.core.Config.AugmentedFaceMode
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+import java.util.*
+
+
+class FaceArFragment : ArFragment() {
+
+    override fun getSessionConfiguration(session: Session?): Config? {
+        val config = Config(session)
+        config.setAugmentedFaceMode(AugmentedFaceMode.MESH3D)
+        return config
+    }
+
+    override fun getSessionFeatures(): Set<Session.Feature?>? {
+        return EnumSet.of(Session.Feature.FRONT_CAMERA)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val frameLayout =
+            super.onCreateView(inflater, container, savedInstanceState) as FrameLayout?
+
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+
+        return frameLayout
+    }
+
+
+}
