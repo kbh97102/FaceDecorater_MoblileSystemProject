e0f824240d5d6ae66c0db96c59c681f727908531
Arakene <kbh97102@naver.com>
2020-11-10 17:49:02 +0900

    Update camera sketch layout

--- app/src/main/java/com/example/facedecorater/camera/CameraSketch.kt
+++ app/src/main/java/com/example/facedecorater/camera/CameraSketch.kt
@@ -1,12 +1,98 @@
 package com.example.facedecorater.camera
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.facedecorater.R
+import kotlinx.android.synthetic.main.camera_sketch_layout.*
+import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
 
 class CameraSketch : AppCompatActivity() {
 
+    private var imageCapture: ImageCapture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_sketch_layout)
 
+        setToolbar()
+        startCamera()
+    }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val executor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture.addListener(Runnable{
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(camera_sketch_previewView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            imageCapture = ImageCapture.Builder().build()
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (exc : Exception){
+                Log.e("Camera Error", "Use case binding failed")
+            }
+        }, executor)
+    }
+
+    private fun takePicture(){
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = File(getOutputDirectory(), SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.KOREA).format(System.currentTimeMillis()) + ".jpg")
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback{
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                Log.d("ImageSaved", "Success")
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Log.e("ImageSaved", "Error")
+            }
+
+        })
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun setToolbar() {
+        camera_sketch_toolbar.apply {
+            title = ""
+            subtitle = ""
+        }
+        setSupportActionBar(camera_sketch_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
