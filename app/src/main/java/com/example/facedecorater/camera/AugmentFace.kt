63c139b049452923eb9fd6e08d8d485631371de0
Arakene <kbh97102@naver.com>
2020-11-17 20:40:42 +0900

    Add arcore

--- app/src/main/java/com/example/facedecorater/camera/AugmentFace.kt
+++ app/src/main/java/com/example/facedecorater/camera/AugmentFace.kt
@@ -5,36 +5,81 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.facedecorater.R
 import com.example.facedecorater.camera.feature.FaceArFragment
 import com.google.ar.core.AugmentedFace
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.Texture
+import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.AugmentedFaceNode
+import java.util.*
 
 class AugmentFace : AppCompatActivity() {
 
-    private lateinit var arFragment: FaceArFragment
-    private lateinit var faceRegionsRenderable: ModelRenderable
-    private lateinit var faceMeshTexture: Texture
-    private val faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
 
+    private var arFragment: FaceArFragment? = null
+
+    private var faceRegionsRenderable: ModelRenderable? = null
+
+    private val faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera_ar)
 
-//
-//        //https://developers.google.com/ar/develop/java/camera-configs
-//
-//
-//        val sharedSession = Session(this, EnumSet.of(Session.Feature.SHARED_CAMERA))
-//        val sharedCamera = sharedSession.sharedCamera
-//        val filter = CameraConfigFilter(sharedSession)
-//        filter.apply {
-//            targetFps = EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30)
-//            depthSensorUsage = EnumSet.of(CameraConfig.DepthSensorUsage.DO_NOT_USE)
-//        }
-//        val cameraConfigList = sharedSession.getSupportedCameraConfigs(filter)
-//        sharedSession.cameraConfig = cameraConfigList[0]
-//        val cameraId = sharedSession.cameraConfig.cameraId
+        arFragment = supportFragmentManager.findFragmentById(R.id.face_fragment) as FaceArFragment
+
+
+        // Load the face regions renderable.
+        // This is a skinned model that renders 3D objects mapped to the regions of the augmented face.
+        ModelRenderable.builder()
+            .setSource(this, R.raw.fox_face)
+            .build()
+            .thenAccept { modelRenderable: ModelRenderable ->
+                faceRegionsRenderable = modelRenderable
+                modelRenderable.isShadowCaster = false
+                modelRenderable.isShadowReceiver = false
+            }
+
+
+        val sceneView: ArSceneView = arFragment!!.arSceneView
+        sceneView.cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
+
+
+        val scene = sceneView.scene
+        scene.addOnUpdateListener { frameTime: FrameTime? ->
+            if (faceRegionsRenderable == null) {
+                return@addOnUpdateListener
+            }
+            val faceList =
+                sceneView.session!!.getAllTrackables(
+                    AugmentedFace::class.java
+                )
+
+            // Make new AugmentedFaceNodes for any new faces.
+            for (face in faceList) {
+                if (!faceNodeMap.containsKey(face)) {
+                    val faceNode = AugmentedFaceNode(face)
+                    faceNode.setParent(scene)
+                    faceNode.faceRegionsRenderable = faceRegionsRenderable
+                    faceNodeMap.put(face, faceNode)
+                }
+            }
+
+            // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
+            val iter: MutableIterator<Map.Entry<AugmentedFace, AugmentedFaceNode>> =
+                faceNodeMap.entries.iterator()
+            while (iter.hasNext()) {
+                val entry =
+                    iter.next()
+                val face = entry.key
+                if (face.trackingState == TrackingState.STOPPED) {
+                    val faceNode = entry.value
+                    faceNode.setParent(null)
+                    iter.remove()
+                }
+            }
+        }
+
 
     }
 }
