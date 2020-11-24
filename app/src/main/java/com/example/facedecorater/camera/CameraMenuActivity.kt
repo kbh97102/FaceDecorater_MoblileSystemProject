f045b7357aeb7f196fc1aae6342649d526e56568
Arakene <kbh97102@naver.com>
2020-11-24 23:55:33 +0900

    Add camera menu activity

---
+++ app/src/main/java/com/example/facedecorater/camera/CameraMenuActivity.kt
@@ -0,0 +1,14 @@
+package com.example.facedecorater.camera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.facedecorater.R
+
+class CameraMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_menu_layout)
+
+    }
+}
