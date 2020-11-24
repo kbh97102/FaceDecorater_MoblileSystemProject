323e469a434207c9e84c431cc7359c2880f04713
Arakene <kbh97102@naver.com>
2020-11-24 23:51:30 +0900

    Update start activity

--- app/src/main/java/com/example/facedecorater/MainActivity.kt
+++ app/src/main/java/com/example/facedecorater/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.facedecorater
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
+import com.example.facedecorater.gallery.GalleryMenuActivity
 import kotlinx.android.synthetic.main.gallery_menu_layout.*
+import kotlinx.android.synthetic.main.start_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.gallery_menu_layout)
-    }
+        setContentView(R.layout.start_activity)
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.gallery_menu, menu)
-        return true
+        gallery_button.setOnClickListener {
+            startActivity(Intent(this, GalleryMenuActivity::class.java))
+        }
+        camera_button.setOnClickListener {
+//            startActivity(Intent(this, ::class.java))
+        }
     }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val menuInflater = menuInflater
+//        menuInflater.inflate(R.menu.gallery_menu, menu)
+//        return true
+//    }
 }
