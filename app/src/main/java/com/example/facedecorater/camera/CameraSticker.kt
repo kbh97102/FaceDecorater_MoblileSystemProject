d8fb05a340974dc89b9dcaa25733ef5f72e9cb65
Arakene <kbh97102@naver.com>
2020-11-25 01:15:27 +0900

    Add camera 2d sticker

---
+++ app/src/main/java/com/example/facedecorater/camera/CameraSticker.kt
@@ -0,0 +1,13 @@
+package com.example.facedecorater.camera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.facedecorater.R
+
+class CameraSticker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_sticker_2d_layout)
+    }
+}
---
+++ app/src/main/java/com/example/facedecorater/camera/feature/Region.kt
@@ -0,0 +1,5 @@
+package com.example.facedecorater.camera.feature
+
+enum class Region {
+    LEFT_EYE, RIGHT_EYE, NOSE, MOUSE
+}
