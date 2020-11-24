32741843e597e77c0173500554bd03e1998692f7
Arakene <kbh97102@naver.com>
2020-11-25 00:02:30 +0900

    Update camera menu

--- app/src/main/java/com/example/facedecorater/camera/CameraMenuActivity.kt
+++ app/src/main/java/com/example/facedecorater/camera/CameraMenuActivity.kt
@@ -1,8 +1,10 @@
 package com.example.facedecorater.camera
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.facedecorater.R
+import kotlinx.android.synthetic.main.camera_menu_layout.*
 
 class CameraMenuActivity : AppCompatActivity() {
 
@@ -10,5 +12,25 @@ class CameraMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera_menu_layout)
 
+        setToolbar()
+
+        camera_menu_sketch_button.setOnClickListener {
+            startActivity(Intent(this, CameraSketch::class.java))
+        }
+        camera_menu_3d_button.setOnClickListener {
+            startActivity(Intent(this, AugmentFace::class.java))
+        }
+        camera_menu_sticker_button.setOnClickListener {
+
+        }
+    }
+
+    private fun setToolbar() {
+        camera_menu_toolbar.apply {
+            title = ""
+            subtitle = ""
+        }
+        setSupportActionBar(camera_menu_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
